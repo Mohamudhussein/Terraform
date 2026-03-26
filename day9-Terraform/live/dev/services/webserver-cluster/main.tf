069b2b5493f25ec1f5457d544ed846189efbb29e
Mohamudhussein <husseinmohamud6075@gmail.com>
2026-03-26 23:12:16 +0300

    Day 9: Webserver cluster module with ALB and ASG

---
+++ day9-Terraform/live/dev/services/webserver-cluster/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "webserver_cluster" {
+source = "github.com/Mohamudhussein/terraform-aws-webserver-cluster?ref=v0.0.2"
+  cluster_name  = "webservers-dev"
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 4
+  server_port   = 8080
+  server_text   = "Hello from the DEV webserver cluster"
+  alb_port      = 80
+  aws_region    = "us-east-1"
+}
+
+output "alb_dns_name" {
+  value = module.webserver_cluster.alb_dns_name
+}
+
+output "asg_name" {
+  value = module.webserver_cluster.asg_name
+}
---
+++ day9-Terraform/live/production/services/webserver-cluster/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "webserver_cluster" {
+  source = "github.com/Mohamudhussein/terraform-aws-webserver-cluster?ref=v0.0.1"
+
+  cluster_name  = "webservers-production"
+  instance_type = "t2.medium"
+  min_size      = 4
+  max_size      = 10
+  server_port   = 8080
+  server_text   = "Hello from the PRODUCTION webserver cluster"
+  alb_port      = 80
+  aws_region    = "us-east-1"
+}
+
+output "alb_dns_name" {
+  value = module.webserver_cluster.alb_dns_name
+}
+
+output "asg_name" {
+  value = module.webserver_cluster.asg_name
+}
